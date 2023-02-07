348bb30699712b2cc084d0a38ffee183bbae3dc5
camilabv14 <camilabv14@hotmail.com>
2023-02-07 06:39:24 -0500

    Update index.js

--- src/callback/index.js
+++ src/callback/index.js
@@ -1,8 +1,11 @@
-function sum (num1, num2) {
-    return num1 + num2;
+function sum(num1, num2) {
+  return num1 + num2;
 }
-function calc (num1, num2, callback) {
-    return callback(num1, num2);
+function calc(num1, num2, callback) {
+  return callback(num1, num2);
 }
 
-console.log(calc(1, 2, sum))
+setTimeout(greeting(), 5000);
+function greeting(name) {
+    console.log(`Hola ${name} Javascript`)
+}
