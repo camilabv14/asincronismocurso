6e5c5a06f12cc11d978e63c21ec30abdac83bf74
camilabv14 <camilabv14@hotmail.com>
2023-02-09 09:18:01 -0500

    callback created

---
+++ src/callback/index.js
@@ -0,0 +1,11 @@
+function sum(num1, num2) {
+  return num1 + num2;
+}
+function calc(num1, num2, callback) {
+  return callback(num1, num2);
+}
+
+setTimeout(greeting(), 5000);
+function greeting(name) {
+  console.log(`Hola ${name} Javascript`);
+}
